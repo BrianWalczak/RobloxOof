8d814f3d8c93d0a4220ae0e5581e2ca11bf1e61a
BrianWalczak <72092838+BrianWalczak@users.noreply.github.com>
2023-01-05 19:13:38 -0600

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,3 @@
+cd /Applications/Roblox.app/Contents/Resources/content/sounds
+rm ouch.ogg
+curl -o ouch.ogg 'https://cdn.discordapp.com/attachments/1002289224445349988/1006256772866117662/ouch.ogg'
